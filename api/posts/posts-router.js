ae74fe48caa0fedadd005cd4e41e4720f3c560e2
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-11-30 16:54:22 -0800

    first endpoint working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,7 +4,45 @@ const Post = require('./posts-model')
 const router = express.Router()
 
 
+// /api/posts
 
+router.get(`/`, async (req, res) => {
+    try {
+        const posts = await Post.find()
+        if (!posts) {
+            res.status(500).json({
+                message: 'No posts to display'
+            })
+        } else {
+            res.status(200).json(posts)
+        }
 
+    } catch (err) {
+        res.status(500).json({
+            message: "The posts information could not be retrieved",
+            error: err.message
+        })
+    }
+})
+
+router.get(`/:id`, async (req, res) => {
+    
+})
+
+router.post(`/`, async (req, res) => {
+    
+})
+
+router.put(`/:id`, async (req, res) => {
+    
+})
+
+router.delete(`/:id`, async (req, res) => {
+    
+})
+
+router.get(`/:id/comments`, async (req, res) => {
+    
+})
 
 module.exports = router

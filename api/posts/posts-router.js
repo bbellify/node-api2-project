de48c574d11dc05471ff44807936c559f4ba50b6
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-11-30 18:37:24 -0800

    put endpoint working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -79,7 +79,34 @@ router.post(`/`, (req, res) => {
 })
 
 router.put(`/:id`, async (req, res) => {
-    
+    const updates = req.body
+    const id = req.params.id
+
+    if (!updates.title || !updates.contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Post.update(id, updates)
+            .then(updated => {
+                if (!updated) {
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist"
+                    })
+                } else {
+                    Post.findById(id)
+                    .then(post => {
+                        res.status(200).json(post)
+                    })
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "The post information could not be modified",
+                    error: err.message
+                })
+            })
+        }
 })
 
 router.delete(`/:id`, async (req, res) => {
